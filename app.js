a759c4c2bd7ee0a514be61563ee61db879bad0cd
Gibran Raydan <superpegorino@gmail.com>
2021-02-05 18:53:33 -0500

    CRUD intento

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const User = require("./models/user");
 const app = express();
+const router = express.Router();
 const port = process.env.PORT;
 
-app.get('/',(req,res) => {
-    res.send('Hello World')
-});
+app.use('/', require('./routes/routes'));
 
-//app.get('/users',async (req,res) => {
-    
-//    u =  await getUser();
-//    u.then( e => console.log(e));
-//    res.send(u);
-//});
+/* app.get('/',(req,res) => {
+    res.send('Hello World')
+}); */
 
 app.listen(port, ()=> {
     console.log('App is listening at http://localhost:'+ port);
 });
 
-async function getUser(){
-    u = await User.findAll();
-    return u;
-};
---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+//User services
+router.post('/', userController.addUser);
+//router.get('/', userController.findUsers);
+router.get('/:id', userController.findUserById);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUserById);
+
+module.exports = router;
